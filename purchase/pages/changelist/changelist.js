58dc4692c8615611982fa70585f3bee1c0e4e1f6
alminChan <fir512@126.com>
2019-01-07 19:08:26 +0800

    滑动索引

--- purchase/pages/changelist/changelist.js
+++ purchase/pages/changelist/changelist.js
@@ -1,24 +1,92 @@
 // pages/changelist/changelist.js
+const utils = require("../../utils/util.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    currentTopItem: 0,
+    slideOffset: 0,
+    tabW: 0,
+    navScrollLeft: 0,
+    windowHeight: 0,
+    pixelRatio: 0,
+    windowWidth: 0,
+    arrowWid: 0,
+    swiperHeight: 0,
+    qSearchHeight: 0,
+    navHeight: 0,
+    maskShow: false,
+    navList: [{
+      name: "全部商品"
+    }, {
+      name: "全部商品1"
+    }, {
+      name: "全部商品2"
+    }, {
+      name: "全部商品3"
+    }, {
+      name: "全部商品4"
+    }, {
+      name: "全部商品5"
+    }, {
+      name: "全部商品6"
+    }, {
+      name: "全部商品7"
+    }, {
+      name: "全部商品8"
+    }, {
+      name: "全部商品9"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.loadTypeDatas(0)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    const that = this;
+    let qArrow = wx.createSelectorQuery();
+    qArrow.select('.arrow').boundingClientRect();
+    qArrow.exec((res) => {
+      if (res[0] === null) return;
+      that.setData({
+        arrowWid: res[0].width
+      });
+      let qSearch = wx.createSelectorQuery();
+      qSearch.select('.input').boundingClientRect();
+      qSearch.exec((res) => {
+        if (res[0] === null) return;
+        that.setData({
+          qSearchHeight: res[0].height
+        })
+        let qNav = wx.createSelectorQuery();
+        qNav.select('.nav-wrap').boundingClientRect();
+        qNav.exec((res) => {
+          if (res[0] === null) return;
+          that.setData({
+            navHeight: res[0].height
+          })
+          wx.getSystemInfo({
+            success: (res) => {
+              this.setData({
+                pixelRatio: res.pixelRatio,
+                windowHeight: res.windowHeight,
+                windowWidth: res.windowWidth - (that.data.arrowWid / 4),
+                swiperHeight: res.windowHeight - that.data.qSearchHeight - that.data.navHeight - 10
+              })
+            },
+          })
+        })
+      })
+    })
 
   },
 
@@ -62,5 +130,48 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  switchNav(e) {
+    var cur = e.currentTarget.dataset.current;
+    //每个tab选项宽度占1/5
+    var singleNavWidth = this.data.windowWidth / 5;
+    //tab选项居中                            
+    this.setData({
+      navScrollLeft: (cur - 2) * singleNavWidth
+    })
+    if (this.data.currentTopItem == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTopItem: cur
+      })
+    }
+    if (!!this.data.maskShow) {
+      this.setData({
+        maskShow: !this.data.maskShow
+      })
+    }
+  },
+  switchTab(e) {
+    var cur = e.detail.current;
+    var singleNavWidth = this.data.windowWidth / 5;
+    this.setData({
+      currentTopItem: cur,
+      navScrollLeft: (cur - 2) * singleNavWidth
+    });
+  },
+
+  tabMaskShowHhide(e) {
+    this.setData({
+      maskShow: !this.data.maskShow
+    })
+  },
+  hideMask(e) {
+    this.setData({
+      maskShow: false
+    })
+  },
+  loadTypeDatas(type){
+    utils.showLoading();
   }
 })
--- purchase/utils/util.js
+++ purchase/utils/util.js
@@ -1,3 +1,4 @@
+const config = require('./config.js');
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -14,6 +15,78 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+//成功提示
+function showSuccess(title = "成功啦", duration = 2500) {
+  wx.showToast({
+    title: title,
+    icon: 'success',
+    duration: (duration <= 0) ? 2500 : duration
+  });
+}
+
+function showFail(title = "失败了", duration = 2500) {
+  wx.showToast({
+    title: title,
+    icon: "none",
+    duration: (duration <= 0) ? 2500 : duration
+  })
+}
+//loading提示
+function showLoading(title = "请稍后", duration = 3000) {
+  wx.showToast({
+    title: title,
+    icon: 'loading',
+    duration: (duration <= 0) ? 3000 : duration
+  });
+}
+//隐藏提示框
+function hideToast() {
+  wx.hideToast();
+}
+
+//显示带取消按钮的消息提示框
+function alertViewWithCancel(title = "提示", content = "消息提示", confirm, showCancel = "true") {
+  wx.showModal({
+    title: title,
+    content: content,
+    showCancel: showCancel,
+    success: function(res) {
+      if (res.confirm) {
+        confirm();
+      }
+    }
+  });
+}
+//显示不取消按钮的消息提示框
+function alertView(title = "提示", content = "消息提示", confirm) {
+  alertViewWithCancel(title, content, confirm, false);
+}
+
+function request(parameters = "", success, method = "GET", header = {}) {
+  wx.request({
+    url: config.BaseURL + (method == "GET" ? "?" : "") + parameters,
+    data: {},
+    method: method, // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    header: header ? header : "application/json", // 设置请求的 header
+    success: function(res) {
+      success(res);
+    },
+    fail: function() {
+      // fail
+    },
+    complete: function() {
+      // complete
+    }
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  showSuccess: showSuccess,
+  showFail: showFail,
+  showLoading: showLoading,
+  hideToast: hideToast,
+  alertViewWithCancel: alertViewWithCancel,
+  alertView: alertView,
+  request: request
 }
